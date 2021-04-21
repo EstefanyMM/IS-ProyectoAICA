a3286edbe9b0c709783b03f7927748936424b044
Estefany <estefanymassiel.31@gmail.com>
2021-04-20 22:23:20 -0600

    nueva carpeta

---
+++ client/src/components/CodigoSeguridad.js
@@ -0,0 +1,138 @@
+import React, { useState, } from 'react'
+import axios from "axios";
+
+const CodigoSeguridad = () => {
+    const [state, setstate] = useState(false);
+    const [user, setUser] = useState(true);
+
+
+    //const [loginStatus, setLoginStatus] = useState("");
+
+    const { correo, codigo } = user;
+
+    const handleInputChange = (e) => {
+
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSutmit = async (e) => {
+        e.preventDefault();
+        console.log(user);
+
+        const res = await axios.post('http://localhost:4000/estudiante/restablecer-contrasena ', user);
+        const data = await res.data;
+
+        if (data.ok) {
+            setUser(true);
+
+        }
+        else {
+            alert('correo codigo incorrecto')
+        }
+
+
+    }
+
+
+    return (
+
+
+
+
+        <div class="container">
+            <div class="row   justify-content-center">
+
+
+                <div class="  col-md-8 py-md-5 offset-md-2">
+
+                    <div class="card text-center ">
+                        <div class="card-header">
+                            Reestablecer su Contraseña
+  </div>
+                        <div class="card-body">
+
+
+
+
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Ingrese su Correo </label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <div id="emailHelp" onChange={handleInputChange} value={correo}
+                                    name="correo" class="form-text"> </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Ingrese su codigo de seguridad </label>
+                                <input type="text" onChange={handleInputChange} value={codigo}
+                                    name="codigo" class="form-control" id="exampleInputPassword1" />
+                            </div>
+
+
+                            <h5 class="card-title"> </h5>
+
+                            <button onClick={handleSutmit} class="btn btn-primary">Enviar</button>
+                        </div>
+                        <div class="card-footer text-muted">
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+
+            {
+                state &&
+                <div class="row   justify-content-center">
+
+
+
+                    <div class="  col-md-8 py-md-5 offset-md-2">
+
+                        <div class="card text-center ">
+
+                            <div class="card-body">
+
+                                <div class="card-header">
+                                    Reestablecer su Contraseña
+                         </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Ingrese su nueva Contraseña</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <div id="emailHelp" class="form-text"> </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Verfique su Contraseña </label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1" />
+                                </div>
+
+
+                                <h5 class="card-title"> </h5>
+
+                                <a href="#" class="btn btn-primary">Guardar</a>
+                            </div>
+                            <div class="card-footer text-muted">
+
+                            </div>
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+            }
+
+        </div>
+
+
+
+    )
+
+}
+export default CodigoSeguridad;
---
+++ client/src/components/EditarPerfilEstudiante.js
@@ -0,0 +1,214 @@
+import React, { useState, useEffect } from 'react'
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { Link, useParams } from 'react-router-dom'
+import { SidebarData } from './SidebarData'
+import '../css/perfilEstudiante.css'
+import { IconContext} from 'react-icons'
+import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCardBody, MDBCardTitle, MDBCardText, 
+MDBDropdown, MDBDropdownItem, MDBDropdownToggle, MDBDropdownMenu } from "mdbreact";
+import axios from 'axios';
+
+
+ const EditarPerfilEstudiante = () => {
+    const [user, setUser] = useState(
+        {
+          id: '',
+          fechaRegistro: '',
+          nombreUsuario: '',
+          createdAt: '',
+          updatedAt: '',
+          PersonaId: '',
+          Persona: {
+            id: '',
+            nombreCompleto: '',
+            numeroIdentidad: '',
+            direccion: '',
+            edad: '',
+            numeroTelefono: '',
+            createdAt: '',
+            updatedAt: '',
+            Correos: []
+          }
+        }
+      );
+    
+        const { nombreUsuario, Persona } = user;
+        const { nombreCompleto, numeroIdentidad, direccion, edad, numeroTelefono} = Persona;
+    
+        useEffect(() => {
+          getUser();
+        }, []);
+      
+        const logout = () =>{
+          localStorage.clear();
+          window.location.href = '/login';
+        }
+    
+        const logoutt = () => {
+          window.location.href = '/perfil-estudiante';
+        }
+    
+        //const {id} = useParams();
+    
+        const getUser = async () => {
+          let { Id } = JSON.parse(localStorage.getItem('user'));
+      
+          const res = await axios.get('http://localhost:4000/estudiante/' + Id);
+          const data = await res.data;
+      
+          setUser(data);
+          console.log(data);
+          console.log(user);
+      
+        }
+    
+        const handleInputChange = (e) => {
+            setUser({
+                ...user,
+                [e.target.name]: e.target.value
+            });
+        }
+    
+        const editarEstudiante = async () => {
+          let { Id } = JSON.parse(localStorage.getItem('user'));
+    
+          const resp1 = await axios.put('http://localhost:4000/persona/' + Id, user);
+          const data1 = await resp1.data;
+    
+          const resp = await axios.put('http://localhost:4000/estudiante/' + Id, user);
+          const data = await resp.data;
+    
+          console.log(data);
+        }
+    
+        const[sidebar, setSidebar]= useState(false)
+    
+        const showSidebar = () => setSidebar(!sidebar)
+    
+        if (JSON.parse(localStorage.getItem('user'))) {
+          const sizeRule = {minHeight: '41rem'}
+          return (
+            <>
+              <IconContext.Provider value={{ color: '#fff' }}>
+      
+                <div className="navbar">
+                  <Link to="#" className='menu-bars'>
+                    <FaIcons.FaBars onClick={showSidebar} />
+                  </Link>
+                  <Link to="/perfil-prueba" className="linkInicio">Inicio
+                  </Link>
+                  <div className="margen"></div>
+                  <MDBDropdown>
+                    <MDBDropdownToggle caret color="ins" className="color">
+                      {nombreUsuario}
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu basic>
+                      <MDBDropdownItem><li onClick={logoutt} className="tamanio">Volver Perfil</li></MDBDropdownItem>
+                      <MDBDropdownItem divider />
+                      <MDBDropdownItem><li onClick={logout} className="tamanio">Cerrar Sesion</li></MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>  
+                </div>
+    
+                
+                <div className="col-lg-12 mx-auto p-3 py-md-5">
+                    <main>
+                    <div className="container-fluid">
+                    <h1 className="mt-4">Editar Perfil</h1>
+                    <div className="row justify-content-md-center row1">
+                    <div className="col-lg-12">
+                    <div className="card col-md-16 my-4">
+                    <div className="card-body">
+                    <h4 className="font-weight-extrabold my-4 card-title">Informacion Personal</h4>
+                    <div className="form=group">
+                        <div className="row justify-content-around">
+                        <div className="col-md-4 offset-md-2">
+                            <label id="exampleInputEmail1">Nombre Completo</label>
+                            <input value={nombreCompleto} type="text" name="nombreCompleto" 
+                            placeholder="HolA" id="exampleInputEmail1"
+                            className="bordeInput1 largo1" onChange={handleInputChange}></input>
+                        </div>
+                        <div className="col-md-4 offset-md-2">
+                            <label>Numero de identidad</label>
+                            <input value={numeroIdentidad} className="bordeInput largo2"
+                                type="text" name="numeroIdentidad" 
+                                onChange={handleInputChange}
+                            ></input>
+                        </div>
+                        </div>
+                        <div className="full"></div>
+                        <div className="row justify-content-around">
+                        <div className="col-md-4 offset-md-2">
+                            <label>Direccion</label>
+                            <input value={direccion} type="text" name="direccion" 
+                            className="bordeInput1 largo1" onChange={handleInputChange}></input>
+                        </div>
+                        <div className="col-md-4 offset-md-2">
+                            <label>Edad</label>
+                            <input value={edad} className="bordeInput largo2"
+                                type="text" name="edad" 
+                                onChange={handleInputChange}
+                            ></input>
+                        </div>
+                        </div>
+                        <div className="full"></div>
+                        <div className="row justify-content-around">
+                        <div className="col-md-4 offset-md-2">
+                            <label>Numero Telefonico</label>
+                            <input value={numeroTelefono} type="text" name="numeroTelefono" 
+                            className="bordeInput1 largo1" onChange={handleInputChange}></input>
+                        </div>
+                        <div className="col-md-4 offset-md-2">
+                            <label>Nombre de Usuario</label>
+                            <input value={nombreUsuario} className="bordeInput largo2"
+                                type="text" name="nombreUsuario" 
+                                onChange={handleInputChange}
+                            ></input>
+                        </div>
+                        </div>
+                        <div className="espacio8"></div>
+                        <button type="button" class="btn btn-success" onClick={editarEstudiante}>Guardar Cambios</button>
+    
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </main>
+                </div>
+    
+      
+                <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                  <ul className='nav-menu-items' onClick={showSidebar}>
+                    <li className="navbar-toggle">
+                      <Link to="#" className='menu-bars'>
+                        <AiIcons.AiOutlineClose />
+                      </Link>
+                    </li>
+                    {SidebarData.map((item, index) => {
+                      return (
+                        <li Key={index} className={item.cName} >
+                          <Link to={item.path}>
+                            {item.icon}
+                            <span>{item.title}</span>
+                          </Link>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </nav>
+              </IconContext.Provider>
+      
+            </>
+      
+          );
+        } else {
+          window.location.href = '/'
+        }
+      
+   
+}
+
+export default EditarPerfilEstudiante;
