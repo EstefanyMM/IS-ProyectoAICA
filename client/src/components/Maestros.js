631d68ff7acf570e5a259f3b6c94155aaa841916
Estefany <estefanymassiel.31@gmail.com>
2021-04-28 18:00:14 -0600

    componentes de administrador

--- client/src/components/Maestros.js
+++ client/src/components/Maestros.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export const Maestros = () => {
+    const listaMaestro = () => {
+        window.location.href = '/lista-maestro';
+      }
+      const crearmaestro = () => {
+        window.location.href = '/crear-maestro';
+      }
     return (
         <div className="text-center" >
             Maestros
+
+            <div><button type="button" class="btn btn-info" onClick={listaMaestro}>Lista de Maestros</button></div>
+            <div><button type="button" class="btn btn-info" onClick={crearmaestro}>Crear Maestro</button></div>
         </div>
     )
 }
--- client/src/components/Usuarios.js
+++ client/src/components/Usuarios.js
@@ -13,12 +13,7 @@ import React from 'react'
       const listaIdiomas = () => {
         window.location.href = '/lista-idioma';
       }
-      const listaMaestro = () => {
-        window.location.href = '/lista-maestro';
-      }
-      const crearmaestro = () => {
-        window.location.href = '/crear-maestro';
-      }
+     
     return (
         <div className="text-center" >
             Usuarios
@@ -26,8 +21,7 @@ import React from 'react'
             <div><button type="button" class="btn btn-info" onClick={crearidioma}>Crear Idioma</button></div>
             <div><button type="button" class="btn btn-info" onClick={listaEstudiante}>Lista de Estudiantes</button></div>
             <div><button type="button" class="btn btn-info" onClick={listaIdiomas}>Lista de Cursos</button></div>
-            <div><button type="button" class="btn btn-info" onClick={listaMaestro}>Lista de Maestros</button></div>
-            <div><button type="button" class="btn btn-info" onClick={crearmaestro}>Crear Maestro</button></div>
+            <i className="fa fa-phone"></i> 
             
         </div>
         
